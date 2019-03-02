3fedeaebe549596c7fff845a20b3c553ba7a2ffd
vatsalsaglani <saglanivatsal@gmail.com>
2019-03-02 17:55:42 +0530

    Changed the folder structure to a package. Added setup and init python files.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='xtract_features',
+    url='https://github.com/vatsalsaglani/xrayimage_extractfeatures',
+    author='Vatsal Saglani',
+    author_email='saglanivatsal@gmail.com',
+    # Needed to actually package something
+    packages=['xtract_features'],
+    # Needed for dependencies
+    install_requires=['numpy', 'pandas', 'pydicom', 'matplotlib', 'scipy', 'pickle', 'opencv-python', 'opencv-python-contrib', 'Pillow', 'scipy', 'scikit-image', 'scikit-learn'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='MIT',
+    # description='An example of a python package from pre-existing code',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
